8ffacd4e398d02d1c9704d13b6199727cd68690d
jeffassis <cmdtjeff@gmail.com>
2023-10-29 11:51:25 -0300

    navbar-dropdown

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home'
+import ContactUs from './components/pages/ContactUs'
+import Consulting from './components/pages/Consulting'
+import Marketing from './components/pages/Marketing'
+import Design from './components/pages/Design'
+import Development from './components/pages/Development'
+import SignUp from './components/pages/SignUp'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/consulting' element={<Consulting />} />
+        <Route path='/marketing' element={<Marketing />} />
+        <Route path='/design' element={<Design />} />
+        <Route path='/development' element={<Development />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
